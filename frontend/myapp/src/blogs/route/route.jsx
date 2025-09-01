51f4e46b414f88a6fe109db013f4c5c40869ae2b
Haihengly <your-email@example.com>
2025-09-01 16:06:19 +0700

    update

--- frontend/myapp/src/blogs/route/route.jsx
+++ frontend/myapp/src/blogs/route/route.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import View from '../page/view';
+import WelcomePage from '../page/welcome';
 
 
 export default function Routee() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<View />} />
+        <Route path="/" element={<WelcomePage />} />
+        <Route path="/store" element={<View />} />
         {/* Add other routes here if needed */}
       </Routes>
     </Router>
