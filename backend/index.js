12df62c559ea94a2495b588000949fe85a6ff093
Haihengly <your-email@example.com>
2025-09-03 09:52:08 +0700

    delete container

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST || 'db',  
   user: process.env.DB_USER || 'hengly',
   password: process.env.DB_PASSWORD || 'hengly168',
-  database: process.env.DB_NAME || 'storedb',
+  database: process.env.DB_NAME || 'blogdb',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
