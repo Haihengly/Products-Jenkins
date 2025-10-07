abe5689bb7304b650d8c2b9aee7fbeaa8b32660b
Haihengly <your-email@example.com>
2025-10-07 15:04:17 +0700

    update

---
+++ frontend/myapp/src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Routee from './blogs/route/route';
+
+function App() {
+  return (
+    <div>
+      <Routee/>
+    </div>
+  );
+}
+
+export default App;
